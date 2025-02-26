adb3f3222dbe7a398df0a8b5b048a7042ef578b4
Rag <ragspatel2006@gmail.com>
2025-02-26 18:01:25 -0500

    Got prediction endpoints to work

--- Backend/App/Api/player_predictions.py
+++ Backend/App/Api/player_predictions.py
@@ -1,3 +1,21 @@
 from fastapi import APIRouter
 from Backend.App.Utils.fetch_data import player_season_stats, player_recent_matches
 from Backend.App.Models.models import PlayerRequest
+
+router = APIRouter(prefix="/player_predictions", tags=["player_predictions"])
+
+@router.post("/season")
+async def get_player_season_stats(request: PlayerRequest):
+    return player_season_stats(
+        request.player, 
+        request.league
+    )
+
+@router.post("/recent")
+async def get_player_recent_matches(request: PlayerRequest):
+    return player_recent_matches(
+        request.player, 
+        request.team,
+        request.league, 
+        3
+    )
--- Backend/App/Api/team_predictions.py
+++ Backend/App/Api/team_predictions.py
@@ -1,3 +1,22 @@
 from fastapi import APIRouter
 from Backend.App.Utils.fetch_data import latest_H2H, recent_matches
 from Backend.App.Models.models import PlayerRequest
+
+router = APIRouter(prefix="/team_predictions", tags=["team_predictions"])
+
+@router.post("/head_to_head")
+async def get_head_to_head(request: PlayerRequest):
+    return latest_H2H(
+        request.team1,
+        request.team2,
+        request.league
+    )
+
+@router.post("/recent")
+async def get_recent_matches(request: PlayerRequest):
+    return recent_matches(
+        request.team1,
+        request.team2,
+        request.league,
+        3
+    )
