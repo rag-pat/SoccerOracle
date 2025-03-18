2e94f86609033da64f5af75f3d3a348a2284644f
Rag <ragspatel2006@gmail.com>
2025-03-17 22:23:07 -0400

    .

--- Backend/App/Ml_Models/player_predictions.py
+++ Backend/App/Ml_Models/player_predictions.py
@@ -1,3 +1,33 @@
 import pandas as pd
 
 from Utils.fetch_data import player_season_stats, player_recent_matches
+
+def data_preprocessing():
+    pass
+
+def goals():
+    pass
+
+def assists():
+    pass
+
+def dribbles():
+    pass
+
+def passes():
+    pass
+
+def tackles():
+    pass
+
+def duels():
+    pass
+
+def fouls():
+    pass
+
+def cards():
+    pass
+
+def clean_sheets():
+    pass
--- Backend/App/Ml_Models/team_predictions.py
+++ Backend/App/Ml_Models/team_predictions.py
@@ -1,3 +1,27 @@
 import pandas as pd
 
 from Utils.fetch_data import team_season_stats, team_recent_matches
+
+def data_preprocessing():
+    pass
+
+def goals():
+    pass
+
+def fouls():
+    pass
+
+def corners():
+    pass
+
+def offsides():
+    pass
+
+def possession():
+    pass
+
+def cards():
+    pass
+
+def passes():
+    pass
