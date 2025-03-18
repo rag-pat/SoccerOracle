0246399fcf15e8b6785fc3501ea7d3b5d13c6839
Rag <ragspatel2006@gmail.com>
2025-03-17 21:47:57 -0400

    .

---
+++ Backend/App/Ml_Models/player_predictions.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+from Utils.fetch_data import player_season_stats, player_recent_matches
---
+++ Backend/App/Ml_Models/team_predictions.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+from Utils.fetch_data import team_season_stats, team_recent_matches
