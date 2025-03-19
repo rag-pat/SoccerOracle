0683cb959de5b8b390f84004798e04fffdfbe8e8
Rag <ragspatel2006@gmail.com>
2025-03-19 12:32:53 -0400

    Player predictions data pre processing done

--- Backend/App/Ml_Models/player_predictions.py
+++ Backend/App/Ml_Models/player_predictions.py
@@ -1,33 +1,71 @@
 import pandas as pd
 
-from Utils.fetch_data import player_season_stats, player_recent_matches
+from ..Utils.fetch_data import player_season_stats, player_recent_matches
+from ..Utils.ids import get_season_year
+from ..Models.models import PlayerRequest
 
-def data_preprocessing():
-    pass
+class PlayerDataProcessor:
+    def __init__(self, player_info: PlayerRequest):
+        self.player_info = player_info
+        self.recent_matches = player_recent_matches(
+            player_info.player_name,
+            player_info.team_name,
+            player_info.league_name,
+            5,
+            get_season_year()
+        )
 
-def goals():
-    pass
+        print(self.recent_matches)
 
-def assists():
-    pass
 
-def dribbles():
-    pass
+    def prepare_goals_df(self):
+        return pd.DataFrame([{
+            'minutes_played': m['games']['minutes_played'] or 0,
+            'totalshots': m['goals']['totalshots'] or 0,
+            'shotsongoal': m['goals']['shotsongoal'] or 0,
+            'passes_total': m['passes']['total'] or 0,
+            'dribbles_attempts': m['dribbles']['attempts'] or 0,
+            'dribbles_success': m['dribbles']['success'] or 0,
+            'goals_total': m['goals']['total'] or 0
+        } for m in self.recent_matches])
 
-def passes():
-    pass
+    def prepare_assists_df(self):
+        return pd.DataFrame([{
+            'minutes_played': m['games']['minutes_played'] or 0,
+            'passes_total': m['passes']['total'] or 0,
+            'passes_accuracy': int(m['passes']['accuracy']) if m['passes']['accuracy'] else 0,
+            'dribbles_attempts': m['dribbles']['attempts'] or 0,
+            'dribbles_success': m['dribbles']['success'] or 0,
+            'totalshots': m['goals']['totalshots'] or 0,
+            'assists': m['goals']['assists'] or 0
+        } for m in self.recent_matches])
 
-def tackles():
-    pass
+    def prepare_dribbles_df(self):
+        return pd.DataFrame([{
+            'dribbles_attempts': m['dribbles']['attempts'] or 0,
+            'minutes_played': m['games']['minutes_played'] or 0,
+            'totalshots': m['goals']['totalshots'] or 0,
+            'passes_total': m['passes']['total'] or 0,
+            'passes_accuracy': int(m['passes']['accuracy']) if m['passes']['accuracy'] else 0,
+            'dribbles_success': m['dribbles']['success'] or 0
+        } for m in self.recent_matches])
 
-def duels():
-    pass
+    def prepare_passes_df(self):
+        return pd.DataFrame([{
+            'minutes_played': m['games']['minutes_played'] or 0,
+            'passes_accuracy': int(m['passes']['accuracy']) if m['passes']['accuracy'] else 0,
+            'dribbles_attempts': m['dribbles']['attempts'] or 0,
+            'totalshots': m['goals']['totalshots'] or 0,
+            'dribbles_success': m['dribbles']['success'] or 0,
+            'passes_total': m['passes']['total'] or 0
+        } for m in self.recent_matches])
 
-def fouls():
-    pass
-
-def cards():
-    pass
-
-def clean_sheets():
-    pass
+    def prepare_tackles_df(self):
+        return pd.DataFrame([{
+            'minutes_played': m['games']['minutes_played'] or 0,
+            'interceptions': m['tackles']['interceptions'] or 0,
+            'dribbles_attempts': m['dribbles']['attempts'] or 0,
+            'dribbles_success': m['dribbles']['success'] or 0,
+            'fouls_committed': m['fouls']['committed'] or 0,
+            'tackles_total': m['tackles']['total'] or 0
+        } for m in self.recent_matches])
