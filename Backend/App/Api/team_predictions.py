97cd7b3edec5294a60d8f185679803e88d4db015
Rag <ragspatel2006@gmail.com>
2025-03-21 17:16:35 -0400

    Fixed input

--- Backend/App/Api/team_predictions.py
+++ Backend/App/Api/team_predictions.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 from Backend.App.Utils.fetch_data import latest_H2H, recent_matches
-from Backend.App.Models.models import PlayerRequest
+from Backend.App.Models.models import TeamsRequest
 
 router = APIRouter(prefix="/team_predictions", tags=["team_predictions"])
 
 @router.post("/head_to_head")
-async def get_head_to_head(request: PlayerRequest):
+async def get_head_to_head(request: TeamsRequest):
     return latest_H2H(
         request.team1,
         request.team2,
@@ -13,7 +13,7 @@ async def get_head_to_head(request: PlayerRequest):
     )
 
 @router.post("/recent")
-async def get_recent_matches(request: PlayerRequest):
+async def get_recent_matches(request: TeamsRequest):
     return recent_matches(
         request.team1,
         request.team2,
